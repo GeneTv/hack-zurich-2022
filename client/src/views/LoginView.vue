1d2460a982b62bc1c10be848376512f995ce35df
Dillan <d@illan.ch>
2022-09-17 03:27:31 +0200

    I made colors and lines

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="page-wrapper">
+    <div class="login-form">
+      <div class="login-form__title-container">
+        <h2 class="login-form__title">Hello Hacker!</h2>
+        <p class="login-form__subtitle">The app requires you to log in to Spotify to continue.</p>
+      </div>
+      
+
+      <div class="form-controls">
+        <button @click="initSpotifyOauthFlow" class="spotify-login">
+          <img src="@/assets/spotify_logo_colored.png" alt="Spotify logo" class="spotify-login__logo" />
+          <span class="spotify-login__text">Connect to Spotify</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const initSpotifyOauthFlow = () => {
+  console.log('Spotify flow');
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #fbfbfb;
+}
+
+.page-wrapper {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.login-form {
+  align-items: center;
+  background-color: #fff;
+  border-radius: 2px;
+  /* box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.1); */
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  justify-content: center;
+  max-width: 500px;
+  padding: 48px 20px;
+  width: 100%;
+}
+
+.login-form__title-container {
+  margin-bottom: 32px;
+}
+.login-form__title {
+  color: #578BF2;
+  margin-bottom: 8px;
+}
+.login-form__subtitle {
+  color: #ABABAC;
+  font-size: 14px;
+}
+
+.spotify-login {
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  display: flex;
+  gap: 8px;
+  justify-content: center;
+  outline: none;
+  padding: 12px;
+}
+
+.spotify-login__logo {
+  height: 20px;
+  width: 20px;
+}
+
+.spotify-login__text {
+  color: #474747;
+  font-size: 14px;
+  line-height: 20px;
+}
+</style>
---
+++ client/src/views/ResultsView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+    <div class="card" v-for="event in events">
+      <h3> {{ event.title }}</h3>
+      <p>Based on <span>{{ event.basedSong.title }}</span></p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    events: [
+      {
+        title: 'HackZurich 2022',
+        basedSong: {
+          title: 'HOT PURSUIT',
+          author: 'JDSLVT'
+        }
+      },{
+        title: 'Funny Farming',
+        basedSong: {
+          title: 'Eye of the tiger ',
+          author: 'Survivor'
+        }
+      }
+    ]
+  })
+}
+</script>
